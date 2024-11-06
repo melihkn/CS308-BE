27dac6cf938ad71ec2cf59d974a0f39927bfa4dd
orkngkc <orkungokce2@gmail.com>
2024-11-06 21:10:29 +0300

    this will run in port 8003

--- dashboards_service/app.py
+++ dashboards_service/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from controllers.discountControllers import router as dashboard_router
 from controllers.orderControllers import router as order_router
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +12,6 @@ app.include_router(order_router, prefix="/api/v1")
 @app.get("/")
 def read_root():
     return {"message": "Sales Manager Dashboard Service"}
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8003, reload=True)
