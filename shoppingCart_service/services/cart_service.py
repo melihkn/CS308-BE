022329db211fed00a47b6348fa0508405e80b5ce
melihkn <melih.sahinbas@sabanciuniv.edu>
2024-11-11 21:46:26 +0300

    http dependency added

--- shoppingCart_service/services/cart_service.py
+++ shoppingCart_service/services/cart_service.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 # Import the ShoppingCart and ShoppingCartItem classes from the models module
 from models.models import ShoppingCart, ShoppingCartItem, Product, CartAdjustment
