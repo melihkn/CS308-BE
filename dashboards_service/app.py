479a79e0b9ca8b133c3f420feb8bcd5e78a915b2
orkngkc <orkungokce2@gmail.com>
2024-11-11 19:24:07 +0300

    added cors middleware for accepting requests from fronend

--- dashboards_service/app.py
+++ dashboards_service/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from controllers.discountControllers import router as dashboard_router
 from controllers.orderControllers import router as order_router
 from controllers.productControllers import router as product_router
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
@@ -14,6 +15,14 @@ app.include_router(product_router, prefix= "/api/v1")
 @app.get("/")
 def read_root():
     return {"message": "Sales Manager Dashboard Service"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8003, reload=True)
