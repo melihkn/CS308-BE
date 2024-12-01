ae41980fc36c6d9a6a185b081c83e4e6a86b4137
orkngkc <orkungokce2@gmail.com>
2024-12-01 23:18:41 +0300

    images added and dbContexts updated for orkun

--- dashboards_service/dbContext.py
+++ dashboards_service/dbContext.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
 # Replace with your actual MySQL connection string
-DATABASE_URL = "mysql+pymysql://root:MelihKN_53@localhost:3306/Myvet_db"
+DATABASE_URL = "mysql+pymysql://root:orkun2003@127.0.0.1:3306/myvet_db"
 
 # Create an engine
 engine = create_engine(DATABASE_URL)
