8eb8ad5596ae4b715ff807c6b7c114d22b65523b
melihkn <melih.sahinbas@sabanciuniv.edu>
2024-12-03 01:24:18 +0300

    review schemas changed minorly and bug fixed on getCalculate due to request body usage in get method

--- Review/review_schemas/schemas.py
+++ Review/review_schemas/schemas.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
-class Review_Response(BaseModel):
+class Review_Request(BaseModel):
     product_id: str  # Required field
     rating: Optional[int] = Field(..., ge=1, le=5)  # Optional field
     comment: Optional[str] = None  # Optional field
 
 class Get_Review_Response(BaseModel):
     product_id: str  # Required field
+
+class Review_Response(BaseModel):
+    review_id: str  # Required field
+    product_id: str  # Required field
+    rating: int  # Required field
+    comment: Optional[str] = None  # Optional field
